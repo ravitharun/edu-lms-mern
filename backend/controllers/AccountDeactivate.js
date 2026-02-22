81d57e346e5307eec2372d6758d6a5fcda390a55
Ravi Tharun <tr565003@gmail.com>
2026-02-22 20:35:14 +0530

    feat(added the activated ft):)

--- backend/controllers/AccountDeactivate.js
+++ backend/controllers/AccountDeactivate.js
@@ -7,14 +7,34 @@ const AccountDeactivate = async (req, res) => {
             console.log({ message: "ID is missing for AccountDeactivate." })
             return res.status(404).json({ message: "ID is missing for AccountDeactivate." })
         }
-        console.log(id, 'id')
         const updated = await User.findOneAndUpdate({ teacher_Id: id }, { AccountStatus: true }, { new: true }                  // options
         )
-        console.log(updated, 'ipdated')
         return res.status(200).json({ message: "ok" })
     } catch (error) {
         console.log('error from the AccountDeactivate api.')
         return res.status(500).json({ message: 'server Error.' })
     }
 }
-module.exports = { AccountDeactivate }
+
+
+
+const UpdateDeactivate = async (req, res) => {
+    try {
+        const { id } = req.body
+        console.log(id,'UpdateDeactivate')
+        if (!id) {
+            console.log({ message: "ID is missing for AccountDeactivate." })
+            return res.status(404).json({ message: "ID is missing for AccountDeactivate." })
+        }
+        const updated = await User.findOneAndUpdate({ teacher_Id: id }, { AccountStatus: false }, { new: true }                  // options
+        )
+        console.log(updated, 'ipdated')
+        return res.status(200).json({ message: updated })
+    } catch (error) {
+        console.log('error from the AccountDeactivate api.')
+        return res.status(500).json({ message: 'server Error.' })
+    }
+}
+
+
+module.exports = { AccountDeactivate, UpdateDeactivate }
