c8c43c72a1a7cd98892889c6d19648fea294423c
Ravi Tharun <tr565003@gmail.com>
2026-03-03 06:58:06 +0530

    added thr backedn router for unassigin the subjects:)

--- backend/routes/AssiginSubjectrouter.js
+++ backend/routes/AssiginSubjectrouter.js
@@ -1,9 +1,10 @@
-const { SubjectAddByteacher, GetallAssignedSubjects, GetSubjectsByclassID } = require("../controllers/AssiginSubjects")
+const { SubjectAddByteacher, GetallAssignedSubjects, GetSubjectsByclassID, UnassiginTeacherBySubjetcts } = require("../controllers/AssiginSubjects")
 
 const express=require("express")
 const authMiddleware = require("../Middleware/Authmiddleware")
 const SubjectTeacher=express.Router()
 SubjectTeacher.post("/assign/subjects",authMiddleware,SubjectAddByteacher)
+SubjectTeacher.delete("/Delete/AssiginSubjects",authMiddleware,UnassiginTeacherBySubjetcts)
 SubjectTeacher.get("/assign/AllSubjects",authMiddleware,GetallAssignedSubjects)
 SubjectTeacher.get("/Get/Subjects/:id",GetSubjectsByclassID)
 module.exports={SubjectTeacher}
