5d4139efe96ad8e6e0893e74e7366756bdf304b9
Ravi Tharun <tr565003@gmail.com>
2026-03-30 10:13:41 +0530

    updated the env server

--- backend/config/email.js
+++ backend/config/email.js
@@ -9,7 +9,10 @@ const transporter = nodemailer.createTransport({
     pass: process.env.Email_pass // App Password (no spaces)
   }
 });
-
+console.log({
+  "user": process.env.Email_User,
+  "pass": process.env.Email_pass // App Password (no spaces)
+})
 transporter.verify()
 
   .then(() => console.log("✅ Gmail SMTP Connected"))
