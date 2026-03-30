aeed87ce9da2ac22b8b0e6e90825862d48bed435
Ravi Tharun <tr565003@gmail.com>
2026-03-30 08:42:16 +0530

    fixing the prod level code

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI); // must be string
+    await mongoose.connect(process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/lmsdb'); // must be string
     console.log("MongoDB connected");
     console.log("MongoDB connected");
 } catch (error) {
