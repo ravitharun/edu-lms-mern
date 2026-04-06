f0ac24fc449aaaa8a3f8ead05d7c1d39cd725cc0
Ravi Tharun <tr565003@gmail.com>
2026-04-06 19:09:46 +0530

    bug fixed in the adminNavbar

--- UI/src/Pages/AdminPages/Master/AddTimeTable.jsx
+++ UI/src/Pages/AdminPages/Master/AddTimeTable.jsx
@@ -95,12 +95,12 @@ function AddTimeTable() {
             <Toaster></Toaster>
             <div className="min-h-screen flex bg-gray-50">
                 {/* Sidebar */}
-                <MasterAdminNavbar path="dashboard" />
+                <MasterAdminNavbar path="TimeTable" />
                 {/* Main Content */}
                 <div className="flex-1 flex flex-col min-w-0 w-full">
 
                     {/* Top Navbar */}
-                    <MasterLogoNav path="TimeTable" />
+                    <MasterLogoNav path="Time Table" />
 
                     {/* Content */}
                     <main className="flex-1 pt-16 pb-8 px-4 md:px-8 lg:px-12 overflow-y-auto">
