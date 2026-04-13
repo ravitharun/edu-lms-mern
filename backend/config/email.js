3ae04ad6b50e9c4780a004a9c56423a089341c1a
Ravi Tharun <tr565003@gmail.com>
2026-04-13 11:22:39 +0530

    updated the email config in prod

--- backend/config/email.js
+++ backend/config/email.js
@@ -3,16 +3,14 @@ const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
   service: "gmail",
-  port: 587,
-  secure: false, 
-
+  port: 465,
+  secure: true, // ✅ correct for 465
   auth: {
     user: process.env.Email_User,
-    pass: process.env.Email_pass 
+    pass: process.env.Email_pass
   },
   family: 4
 });
-
 transporter.verify()
 
   .then(() => console.log("✅ Gmail SMTP Connected"))
