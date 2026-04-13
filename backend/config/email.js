f041bd6461f85c38244c02e2f34aa3062f36286f
Ravi Tharun <tr565003@gmail.com>
2026-04-13 11:37:33 +0530

    added the test email

--- backend/config/email.js
+++ backend/config/email.js
@@ -1,18 +1,32 @@
 const nodemailer = require("nodemailer");
 
-
+console.log({
+  user: process.env.Email_User,
+  pass: process.env.Email_pass
+})
 const transporter = nodemailer.createTransport({
-  service: "gmail",
-  port: 465,
-  secure: true, // ✅ correct for 465
-  auth: {
+  service: "smtp.gmail.com",
+  port: 587,
+  secure: false, auth: {
     user: process.env.Email_User,
     pass: process.env.Email_pass
   },
-  family: 4
+  family: 4, logger: true,
+  debug: true,
 });
 transporter.verify()
 
   .then(() => console.log("✅ Gmail SMTP Connected"))
   .catch(err => console.error("❌ Error:", err));
+const test = async () => {
+  console.log("test email .");
+
+  await transporter.sendMail({
+    from: "tr565003@gmail.com",
+    to: "tharunravi6722@gmail.com",
+    subject: "Test",
+    text: "Working ✅",
+  });
+}
+test()
 module.exports = { transporter }
