0e690a13a3109d6e30506e0a9f4eff48828cac88
Ravi Tharun <tr565003@gmail.com>
2026-07-12 13:19:41 +0530

    feat: add student marks upload interface with subject-wise entry

--- backend/controllers/AttandanceController.js
+++ backend/controllers/AttandanceController.js
@@ -4,16 +4,16 @@ const GetStudents = async (req, res) => {
     try {
         const { ClassID } = req.query;
 
-        let format = ClassID
+
+        console.log(ClassID, ' req.query');
+
 
         if (!ClassID) {
             return res.status(404).json({ message: "SomeThing went Wrong." })
         }
-        // CSE3-CSE-3
-        // console.log(format.split("-")[1] + " " + format.split("-")[2])
-        // EEE 2
-        const response = await User.find({ StudentsYearDepartment: format.split("-")[1] + " " + format.split("-")[2] })
-        console.log(response,'response')
+
+        const response = await User.find({ StudentsYearDepartment: ClassID }).select("name email  isActive Student_ID department  StudentsYearDepartment AccountStatus profilePreview")
+        console.log(response, 'response')
 
         return res.status(200).json({ message: response })
     } catch (error) {
