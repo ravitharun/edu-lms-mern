2420d1db3ba98eeebf93d19d7b9b4054cd1c27e1
Ravi Tharun <tr565003@gmail.com>
2026-07-17 15:27:08 +0530

    updated the ui

--- backend/controllers/Marks.js
+++ backend/controllers/Marks.js
@@ -5,28 +5,39 @@ const AssiginMarks = async (req, res) => {
     try {
 
         const { data } = req.body
-        console.log(data, 'data');
         const Save_studentMarks = await StudentMarks.insertMany(data)
 
-        return res.status(201).json({ message: 'Marks Saved...',status:true })
+        return res.status(201).json({ message: 'Marks Saved...', status: true })
 
 
     } catch (error) {
         console.log(error.message, 'check err ');
 
-        return res.status(500).json({ message: 'server error..',status:!1 })
+        return res.status(500).json({ message: 'server error..', status: !1 })
     }
 }
 
 
-const fetchMarksByStudentId=async(req,res)=>{
+const fetchMarksByStudentId = async (req, res) => {
     try {
-        
-        return res.status(200).json({ message: 'Data Fetched...',status:true })
+        const { Section } = req.query
+        console.log(Section, 'Section');
+        // console.log(Section.trim(),'Section.trim()');
+
+
+        const data = await StudentMarks.find({ Semester: Section }).populate("id")
+        // .populate("subjectid")
+        console.log(data[0].subjectid, 'wdw');
+        console.log(data, 'tharuns')
+
+
+        return res.status(200).json({ message: 'Data Fetched...', data: data, status: true })
     } catch (error) {
-        return res.status(500).json({ message: 'server error..',status:!1 })
-        
+        console.log(error.message);
+
+        return res.status(500).json({ message: 'server error..', status: false })
+
     }
 }
 
-module.exports = {AssiginMarks,fetchMarksByStudentId}
+module.exports = { AssiginMarks, fetchMarksByStudentId }
--- backend/models/MarksSchema.js
+++ backend/models/MarksSchema.js
@@ -1,23 +1,23 @@
 const mongoose = require("mongoose")
 const StudentMarks = new mongoose.Schema({
-    // id
-    id: { type: mongoose.Schema.ObjectId, required: true },
-    // Mjp001
-    subjectid: { type: mongoose.Schema.ObjectId, required: true },
-    // cse-8
+
+    id: { type: mongoose.Schema.ObjectId, required: true, ref: "User" },
+
+    subjectid: { type: mongoose.Schema.ObjectId, required: true, ref:"SubjectWiseTeacher" },
+
     Semester: { type: String, required: true },
-    // 32
+
     internal: { type: Number, default: 0 },
-    // 22
+
     lab: { type: String, default: 0 },
-    // intrn+lab+fina/3
+
     final: { type: String, default: 0 },
-    // Total based %
+
     percentage: { type: Number, default: 0 },
-    // Total based Grade
+
     Grade: { type: String, default: '' },
-   
-    total: { type:Number, default: 0 }
+
+    total: { type: Number, default: 0 }
 
 
 
